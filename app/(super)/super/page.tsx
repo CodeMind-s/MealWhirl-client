6673fb862db24552fd9abe8dc229b4777cb09ced
Randini Maliksha <randini.maliksha@gmail.com>
2025-04-28 00:09:37 +0530

    feat: add login api

--- app/(super)/super/page.tsx
+++ app/(super)/super/page.tsx
@@ -1,5 +1,17 @@
-import { SuperAdminDashboard } from "@/components/super/super-admin-dashboard"
+import { SuperAdminDashboard } from "@/components/super/super-admin-dashboard";
+import { ThemeProvider } from "@/components/driver/theme-provider";
+import { AuthProvider } from "@/contexts/auth-context";
 
 export default function Home() {
-  return <SuperAdminDashboard />
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <AppContent />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
+
+function AppContent() {
+  return <SuperAdminDashboard />;
 }
