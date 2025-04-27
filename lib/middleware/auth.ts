ed30be82b53812cfc2121e91d88317dc67fa4919
bhashanasirimanna <bhashanasirimanna@gmail.com>
2025-04-27 23:00:11 +0530

    feat: implement token management functions for local storage

---
+++ lib/middleware/auth.ts
@@ -0,0 +1,27 @@
+export const getToken = async () => {
+  try {
+      const token = localStorage.getItem('accessToken');
+      return token;
+      
+  } catch (error) {
+      console.log('Error retrieving token:', error);
+      return null;
+  }
+};
+
+export const setToken = (token: string) => {
+  try {
+      localStorage.setItem('accessToken', token);
+      
+  } catch (error) {
+      console.log('Error storing token:', error);
+  }
+};
+
+export const removeToken = () => {
+  try {
+      localStorage.removeItem('accessToken');
+  } catch (error) {
+      console.log('Error removing token:', error);
+  }
+};
