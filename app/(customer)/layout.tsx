f578dc731a2b23dc989c746f1cb9e42183dc382b
bhashanasirimanna <bhashanasirimanna@gmail.com>
2025-05-12 18:36:27 +0530

    refactor: comment out HTML structure and wrap layout in ProtectedRoute for role-based access control

--- app/(customer)/layout.tsx
+++ app/(customer)/layout.tsx
@@ -6,6 +6,7 @@ import MainNav from "@/components/main-nav";
 import Footer from "@/components/footer";
 import "./globals.css";
 import { PlacedOrderProvider } from "@/contexts/placed-order-context";
+import ProtectedRoute from "@/components/ProtectedRoute";
 
 export default function RootLayout({
   children,
@@ -13,8 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="min-h-screen flex flex-col">
+    // <html lang="en">
+      // <body className="min-h-screen flex flex-col">
+      <>
+      <ProtectedRoute allowedRoles={["Customer"]}>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
@@ -30,8 +33,10 @@ export default function RootLayout({
             </CartProvider>
           </PlacedOrderProvider>
         </ThemeProvider>
-      </body>
-    </html>
+      </ProtectedRoute>
+      </>
+      // </body>
+    // </html>
   );
 }
 
