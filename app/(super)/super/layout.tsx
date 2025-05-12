7abe10b57b4d4a904aac5ad55fe68fc6b4fbfdf1
bhashanasirimanna <bhashanasirimanna@gmail.com>
2025-05-12 18:36:52 +0530

    refactor: prepare layout for role-based access control by commenting out AuthProvider and ProtectedRoute

--- app/(super)/super/layout.tsx
+++ app/(super)/super/layout.tsx
@@ -3,6 +3,7 @@ import ClientLayout from "./client-layout";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/auth-context";
+import ProtectedRoute from "@/components/ProtectedRoute";
 
 export default function RootLayout({
   children,
@@ -11,10 +12,12 @@ export default function RootLayout({
 }) {
   return (
     <>
-      <AuthProvider>
-        <ClientLayout>{children}</ClientLayout>
-      </AuthProvider>
-      <Toaster />
+      {/* <ProtectedRoute allowedRoles={["Admin"]}> */}
+        {/* <AuthProvider> */}
+          <ClientLayout>{children}</ClientLayout>
+        {/* </AuthProvider> */}
+        <Toaster />
+      {/* </ProtectedRoute> */}
     </>
   );
 }
