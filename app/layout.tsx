3e3c2498dcfdc1d8c442aadd92d80b0c8eaeb2e3
bhashanasirimanna <bhashanasirimanna@gmail.com>
2025-05-12 18:36:02 +0530

    feat: add metadata object and enhance RootLayout structure with ThemeProvider and AuthProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,35 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
+import { AuthProvider } from "@/contexts/auth-context";
+import { ReactNode } from "react";
+import '../styles/globals.css';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="en">
-            <body>
-                {children}
-                <Toaster />
-            </body>
-        </html>
-    );
+export const metadata = {
+  title: "MealWhirl",
+  description: "MealWhirl",
+  generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body lang="en" suppressHydrationWarning>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          <AuthProvider>
+            {children}
+            <Toaster />
+          </AuthProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
