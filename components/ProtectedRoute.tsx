7e7593dcb6d178f0c210882d642800440449baac
bhashanasirimanna <bhashanasirimanna@gmail.com>
2025-05-12 22:49:22 +0530

    feat: add loading state handling in ProtectedRoute for improved user experience

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -1,3 +1,37 @@
+// 'use client';
+
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+// import { useAuth } from "@/contexts/auth-context";
+
+// type ProtectedRouteProps = {
+//   allowedRoles: string[]; // e.g., ['Admin'], ['Customer'], ['Driver']
+//   children: React.ReactNode;
+// };
+
+// export default function ProtectedRoute({ allowedRoles, children }: ProtectedRouteProps) {
+//   const { user } = useAuth();
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     if (!user) {
+//       router.push("/login");
+//     } else if (!allowedRoles.includes(user.type) && !(user.isAdmin )) {
+//       // Redirect unauthorized user
+//       router.push("/login");
+//     }
+//   }, [user, allowedRoles, router]);
+
+//   // Optional: show nothing or a loader while user is being validated
+//   if (!user || (!allowedRoles.includes(user.type) && !(user.isAdmin))) {
+//     return null;
+//   }
+
+//   return <>{children}</>;
+// }
+
+
+
 'use client';
 
 import { useEffect } from "react";
@@ -5,26 +39,31 @@ import { useRouter } from "next/navigation";
 import { useAuth } from "@/contexts/auth-context";
 
 type ProtectedRouteProps = {
-  allowedRoles: string[]; // e.g., ['Admin'], ['Customer'], ['Driver']
+  allowedRoles: string[];
   children: React.ReactNode;
 };
 
 export default function ProtectedRoute({ allowedRoles, children }: ProtectedRouteProps) {
-  const { user } = useAuth();
+  const { user, isLoading } = useAuth();
   const router = useRouter();
 
   useEffect(() => {
-    if (!user) {
-      router.push("/login");
-    } else if (!allowedRoles.includes(user.type) && !(user.isAdmin )) {
-      // Redirect unauthorized user
-      router.push("/login");
+    if (!isLoading) {
+    
+      if (!user) {
+        router.push("/login");
+      } else if (!allowedRoles.includes(user.type) && !user.isAdmin) {
+        router.push("/login");
+      }
     }
-  }, [user, allowedRoles, router]);
+  }, [user, isLoading, allowedRoles, router]);
 
-  // Optional: show nothing or a loader while user is being validated
-  if (!user || (!allowedRoles.includes(user.type) && !(user.isAdmin))) {
-    return null;
+  if (isLoading) {
+    return <div>Loading...</div>; // Or your custom loader
+  }
+
+  if (!user || (!allowedRoles.includes(user.type) && !user.isAdmin)) {
+    return null; // The useEffect will handle the redirect
   }
 
   return <>{children}</>;
