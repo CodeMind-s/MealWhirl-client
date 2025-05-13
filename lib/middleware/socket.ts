89112098db7524418d74efc6c00437f7c9cc44ad
Thimesha Ansar <101446688+thimeshaA@users.noreply.github.com>
2025-05-13 21:31:51 +0530

    feat: replace Socket.IO with WebSocket for order-specific connection handling

--- lib/middleware/socket.ts
+++ lib/middleware/socket.ts
@@ -1,7 +1,4 @@
-import { io } from 'socket.io-client';
 
-
-// const socket = io(process.env.NEXT_PUBLIC_API_URL_SOCKET); // Adjust the URL to your backend
-const socket = io("ws://localhost:4000?orderId=6822fb47dc8d1658a9850279"); // Adjust the URL to your backend
-
-export default socket;
+export const createWebSocketConnection = (orderId: string) => {
+    return new WebSocket(`ws://localhost:4000?orderId=${orderId}`);
+  };
